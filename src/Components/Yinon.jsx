4787a6d23a95212a1d80af40a1d47714aa3dd09c
Yoni <yonatansamfisher@gmail.com>
2021-07-07 22:14:16 +0300

    new updates

--- src/Components/Yinon.jsx
+++ src/Components/Yinon.jsx
@@ -1,26 +1,68 @@
-import React,{useState,useRef} from 'react'
+import React,{useState,useRef,useReducer} from 'react'
+import { useEffect } from 'react';
 import TextInput from './TextInput';
+
+// const state = 0;
+// const reducer=(state , action)=>{
+//     switch(action){
+//         case "up":
+//             return state="UP";
+//         case "down":
+//             return state="DOWN";
+//         case "reset":
+//             return state="RESET";
+//         default:
+//             return state;
+//     }
+// }
+
+// const state = 0;
+// const reducer =(action,state)=>{
+//     switch(action){
+//         case "up":
+//             return state =state+1;
+//         case "down":
+//             return state = state-1;
+//         case "reset":
+//             return state=0;
+//         default:
+//             return state;
+//     }
+// }
 
 export default function Yinon() {
 
-    function CountState(){
-    const [count,setCount] = useState(0);
+    // const [count,dispatch] = useReducer(reducer ,state)
+    const buttonColor = useRef();
+    const inputRef = useRef();
 
-    function upCount(){
-        setCount(count+1)
-    }
 
-    function resetNum(){
-        setCount(0)
+
+    useEffect(()=>{
+//  inputRef.current.focus();
+
+    },[])
+    const Handler=(e)=>{
+        inputRef.current.focus();
     }
-    return {count , upCount , resetNum}
+    function getInput(){
+      buttonColor.current.innerHTML = "<div>Hello World</div>"
+inputRef.current.value = "yitzhak"
     }
+    // const [count,dispatch] = useReducer(reducer,state)
+    
     
     
     return (
         <div>
-
-            <TextInput CountState={CountState}/>
+            <div ref={buttonColor} >This is paragraph</div>
+            <input type="text"  ref={inputRef} />
+            <button onClick={getInput}>useRef</button>
+            {/* <p>{count}</p>
+            <button onClick={()=>dispatch("up")}>UP</button>
+            <button onClick={()=>dispatch("down")}>DOWN</button>
+            <button onClick={()=>dispatch("reset")}>RESET</button>
+            <TextInput CountState={CountState}/> */}
             
         </div>
     )
